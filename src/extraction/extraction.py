3c76183ee341a5403063bf8e4293a978550da3af
natansales221 <natansales221@gmail.com>
2026-08-10 16:33:37 -0300

    improving loggers

--- src/extraction/extraction.py
+++ src/extraction/extraction.py
@@ -2,11 +2,40 @@ from pathlib import Path
 from datetime import datetime
 import pandas as pd
 import requests
+import logging
+
 
 ano_atual = datetime.now().year
 data_hoje = datetime.now().strftime("%m-%d-%Y")
 
 moedas = ["USD", "EUR", "GBP", "CHF", "JPY"]
+
+# Momento da execução
+agora = datetime.now()
+
+data_execucao = agora.strftime("%Y-%m-%d")
+hora_execucao = agora.strftime("%H%M%S")
+
+# logs/2026-08-10/
+pasta_logs = Path("logs") / data_execucao
+pasta_logs.mkdir(parents=True, exist_ok=True)
+
+# logs/2026-08-10/extraction_162530.log
+arquivo_log = pasta_logs / f"extraction_{hora_execucao}.log"
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s | %(levelname)s | %(message)s",
+                    handlers=[
+                        logging.FileHandler(arquivo_log, encoding="utf-8"),
+                        logging.StreamHandler()
+                        ]
+                    )
+
+logger = logging.getLogger(__name__)
+
+logger.info("=" * 60)
+logger.info("INÍCIO DA EXTRAÇÃO PTAX")
+logger.info(f"Arquivo de log: {arquivo_log}")
+logger.info("=" * 60)
 
 URL = (
     "https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/"
@@ -14,6 +43,8 @@ URL = (
 )
 for moeda in moedas:
     for ano in range(2000, ano_atual + 1):
+        logger.info("=" * 60)
+        logger.info(f"Iniciando extração: moeda={moeda}, ano={ano}")
         PARAMS = {
             "@moeda": f"'{moeda}'",
             "@dataInicial": f"'01-01-{ano}'",
@@ -25,18 +56,24 @@ for moeda in moedas:
 
         response = requests.get(URL, params=PARAMS, timeout=30)
         response.raise_for_status()
-
+        logger.info(f"Consulta feita com sucesso!")
         dados = response.json()["value"]
 
         df = pd.DataFrame(dados)
-
+        
+        logger.info(f"Dataframe criado com sucesso")
+        
         destino = Path(f"data/downloads/ptax/teste/{moeda}")
         destino.mkdir(parents=True, exist_ok=True)
-
+        logger.info(f"Pasta criada com sucesso")
         arquivo = destino / f"ptax_{moeda}_{ano}.csv"
 
         df.to_csv(arquivo, index=False, encoding="utf-8")
+        
+        logger.info("Extração concluída")
+        logger.info(f"Foram extraídos {len(df)} registros")
+        logger.info("=" * 60)
 
-        print(f"Extração concluída.")
-        print(f"Registros extraídos: {len(df)}")
-        print(f"Arquivo criado: {arquivo}")
+logger.info("=" * 60)
+logger.info("FIM DA EXTRAÇÃO PTAX")
+logger.info("=" * 60)
